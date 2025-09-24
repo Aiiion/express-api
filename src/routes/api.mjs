c4dfcae1c0444022f2f99ed29184b845e98b2528
Alex <alex.soderberg97@gmail.com>
2025-09-25 01:20:16 +0200

    adds additional routes

--- src/routes/api.mjs
+++ src/routes/api.mjs
@@ -1,11 +1,12 @@
-import { test, weather } from '../controllers/api.controller.mjs';
+import { contact, test, pollution, weather } from '../controllers/api.controller.mjs';
 import { Router } from "express";
 import { cache } from '../middleware/cache.mjs';
 
 const router = Router();
 
-router.get("/", test);
-
+router.get("/", contact);
+router.get("/test", test);
 router.post("/weather", cache(60 * 10), weather);
+router.post("/pollution", cache(60 * 10), pollution);
 
 export default router;
