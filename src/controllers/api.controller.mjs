df576e5a3071d5c814f12c807c71e469006a73a9
Alex <alex.soderberg97@gmail.com>
2025-09-25 01:09:25 +0200

    more DRY and adds error msg

--- src/controllers/api.controller.mjs
+++ src/controllers/api.controller.mjs
@@ -1,17 +1,23 @@
 import axios from "axios";
 import { EXTERNAL_WEATHER_URL } from "../utils/constants.mjs";
+
 export const test = (req, res) => res.status(200).send({message: 'test successful'});
 
 export const weather = async (req, res) => {
     if(!req.query.lat || !req.query.lon)
         return res.status(400).send({message: "You need to provide both lat and lon coordinates"});
-    console.log(req.query.lat, req.query.lon, process.env.WEATHER_API_KEY)
+
+    const appid = process.env.WEATHER_API_KEY;
+
+    if(!appid)
+        return res.status(500).send({message: "API key missing from environment variables"});
+
     const weatherReq = await axios({
         method: 'get',
         url: EXTERNAL_WEATHER_URL + '/weather',
         params: {
             ...req.query,
-            appid: process.env.WEATHER_API_KEY
+            appid
         }
     });
 
@@ -20,7 +26,7 @@ export const weather = async (req, res) => {
         url: EXTERNAL_WEATHER_URL + '/forecast',
         params: {
             ...req.query,
-            appid: process.env.WEATHER_API_KEY
+            appid
         }
     });
 
