15a9e0d297c334af3b20a2f69efeeb40db830a95
Alex <alex.soderberg97@gmail.com>
2025-09-25 01:09:36 +0200

    adds new controller method

--- src/controllers/api.controller.mjs
+++ src/controllers/api.controller.mjs
@@ -35,3 +35,36 @@ export const weather = async (req, res) => {
         forecast: forecastReq.data
     }});
 }
+
+export const pollution = async (req, res) => {
+    if(!req.query.lat || !req.query.lon)
+        return res.status(400).send({message: "You need to provide both lat and lon coordinates"});
+
+    const appid = process.env.WEATHER_API_KEY;
+
+    if(!appid)
+        return res.status(500).send({message: "API key missing from environment variables"});
+
+    const pollutionReq = await axios({
+        method: 'get',
+        url: EXTERNAL_WEATHER_URL + '/pollution',
+        params: {
+            ...req.query,
+            appid
+        }
+    });
+
+    const forecastReq = await axios({
+        method: 'get',
+        url: EXTERNAL_WEATHER_URL + '/pollution/forecast',
+        params: {
+            ...req.query,
+            appid
+        }
+    });
+
+    return res.status(200).send({data: {
+        current: pollutionReq.data,
+        forecast: forecastReq.data
+    }});
+}
