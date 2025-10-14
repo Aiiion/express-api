16292d1621ee565b4165c9c0e01b05219707aae9
Alex <alex.soderberg97@gmail.com>
2025-10-14 20:59:59 +0200

    add cors policy

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from "./routes/index.mjs";
-
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors({
+  origin: '*'
+}));
 
 const port = process.env.PORT || 3000;
 
