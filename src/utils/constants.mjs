1e74ad13ba8bb77decc21a4bcf8f001681eb7377
Alex <alex.soderberg97@gmail.com>
2026-01-15 22:29:19 +0100

    use https

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,5 +1,5 @@
 export const OWM_API_URL = "https://api.openweathermap.org/data/2.5";
 export const SMHI_WPT_API_URL = "https://wpt-a.smhi.se/backend-weatherpage";
-export const WEATHERAPI_API_URL = "http://api.weatherapi.com/v1";
+export const WEATHERAPI_API_URL = "https://api.weatherapi.com/v1";
 export const EMAIL = "alex@bierhance.se";
 export const GITHUB = "https://github.com/Aiiion/express-api";
