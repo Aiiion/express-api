5f36624278f6c4e6c677efc4eedf4f1dae75624e
Alex <alex.soderberg97@gmail.com>
2026-01-15 14:17:30 +0100

    adds severitydesciption for foreign residents

--- src/dtos/smhi.dto.mjs
+++ src/dtos/smhi.dto.mjs
@@ -5,6 +5,7 @@ const smhiDto = {
 
     return {
       severity: data.inner.level || null,
+      severityDescription: describeSeverity(data.inner.level) || null,
       description: data.inner.en || null,
       type: data.inner.type || null,
       warningsCount: data.inner.warningsCount || 0,
@@ -12,5 +13,20 @@ const smhiDto = {
     };
   },
 };
+
+const describeSeverity = (severity) => {
+  switch (severity) {
+    case 'YELLOW':
+      return "Certain risks to the public. Disruptions to some societal functions. Take extra care - especially at places more susceptible to changing weather conditions";
+    case 'ORANGE':
+      return "Danger to the public. Disruptions to societal functions.Avoid exposure to the weather conditions.";
+    case 'RED':
+      return "Great danger to the public. Extensive disruptions to societal functions. Avoid all exposure to the weather conditions!";
+    case 'NONE':
+      return "No warnings in effect.";
+    default:
+      return "Unknown";
+  }
+}
 
 export default smhiDto;
