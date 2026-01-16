cb4feeb93b84c968765c111c58c92d7dc03a7d82
Alex <alex.soderberg97@gmail.com>
2026-01-16 15:52:05 +0100

    use forwarded to prevent getting dockers ip

--- src/controllers/info.controller.mjs
+++ src/controllers/info.controller.mjs
@@ -20,8 +20,7 @@ export const cv = (req, res) => {
 };
 
 export const ipLocation = async (req, res) => {
-    const ip = req.query.ip || req.connection.remoteAddress || req.headers['x-forwarded-for'] || req.ip;
- console.log(req.query.ip, req.connection.remoteAddress, req.headers['x-forwarded-for'], req.ip);
+    const ip = req.query.ip || req.headers['x-forwarded-for'] || req.ip;
     if (!net.isIP(ip)) {
         return res.status(400).send({
             error: 'Invalid IP address provided'
