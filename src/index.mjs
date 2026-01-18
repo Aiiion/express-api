90649d489299bca43235c74cc34187f5b07faf0f
Alex <alex.soderberg97@gmail.com>
2026-01-18 22:35:34 +0100

    adds postgress db to project

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import routes from "./routes/index.mjs";
 import cors from 'cors';
+import { connect } from './services/db.service.mjs';
 dotenv.config();
 
 const app = express();
@@ -14,9 +15,21 @@ app.use(cors({
 
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => {
-    console.log(`server running on port ${port}.`);
-});
+let server;
+
+const start = async () => {
+  try {
+    await connect();
+    server = app.listen(port, () => {
+      console.log(`server running on port ${port}.`);
+    });
+  } catch (err) {
+    console.error('Failed to start app due to DB error');
+    process.exit(1);
+  }
+};
+
+start();
 
 export { server };
 export default app;
