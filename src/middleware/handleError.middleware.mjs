13941b12d09d8b951fb67c53f19727d6bd8cd0ca
Alex <alex.soderberg97@gmail.com>
2026-01-21 21:46:49 +0100

    fixes headerSent issue + tries to get code from error

--- src/middleware/handleError.middleware.mjs
+++ src/middleware/handleError.middleware.mjs
@@ -1,7 +1,9 @@
 export const handleError = (err, req, res, next) => {
     console.error('Error occurred:', err.stack || err);
-    return res.status(500).json({ 
-        code: 500,
+    if (res.headersSent) return next(err);
+    const statusCode = err.status || err.statusCode || 500;
+    return res.status(statusCode).json({ 
+        code: statusCode,
         message: err.message, 
     });
 }
