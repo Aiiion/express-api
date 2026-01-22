ad1234fbaf7c8d9cab4af455d0c88feba2793aa7
Alex <alex.soderberg97@gmail.com>
2026-01-22 16:34:01 +0100

    use const for cosistent use of ip for tests etc

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -2,4 +2,5 @@ export const OWM_API_URL = "https://api.openweathermap.org/data/2.5";
 export const SMHI_WPT_API_URL = "https://wpt-a.smhi.se/backend-weatherpage";
 export const WEATHERAPI_API_URL = "https://api.weatherapi.com/v1";
 export const EMAIL = "alex@bierhance.se";
+export const exampleIp = "94.253.32.54"
 export const GITHUB = "https://github.com/Aiiion/express-api";
