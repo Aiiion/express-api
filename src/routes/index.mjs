0395b27df545619f6c49d59c002e4ae7d660e09d
Alex Bierhance <alex.bierhance@gmail.com>
2026-04-14 13:52:22 +0200

    rename api router to weather

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import apiRouter from "./api.mjs";
+import weatherRouter from "./weather.mjs";
 import infoRouter from "./info.mjs"
 import authRouter from "./auth.mjs";
 
 const router = Router();
 
-router.use(apiRouter);
+router.use(weatherRouter);
 router.use(infoRouter);
 router.use(authRouter);
 
