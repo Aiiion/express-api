a7b73f495aacf0090d322a1af6212d14f7ca8456
Alex <alex.soderberg97@gmail.com>
2026-05-26 21:51:43 +0200

    update folder structure and make some reoutes more accesible by cors

--- src/routes/v1/weather.route.mjs
+++ src/routes/v1/weather.route.mjs
@@ -1,11 +1,15 @@
-import { index as v1WeatherIndex } from '../controllers/v1/weather.controller.mjs';
+import { index as v1WeatherIndex } from '../../controllers/v1/weather.controller.mjs';
 import { Router } from "express";
-import { cache } from '../middleware/cache.middleware.mjs';
-import { latLonValidationSchema } from '../utils/validationSchemas.mjs';
+import { cache } from '../../middleware/cache.middleware.mjs';
+import { latLonValidationSchema } from '../../utils/validationSchemas.mjs';
 import { checkSchema } from 'express-validator';
-import { hasOwmKey, validateResult } from '../middleware/validation.middleware.mjs';
+import { hasOwmKey, validateResult } from '../../middleware/validation.middleware.mjs';
+import cors from 'cors';
 
 const router = Router();
+
+router.use('/v1/weather', cors({ origin: '*' }));
+
 const weatherMiddleware = [
     checkSchema(latLonValidationSchema),
     validateResult,
